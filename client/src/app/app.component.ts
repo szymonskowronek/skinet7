974bee68ad98916019711ee86781d87b051d2a9d
Szymon Skowronek <szymon.skowronek@globallogic.com>
2023-11-08 11:55:03 +0100

    End of Section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: Product[] = [];
+
+  constructor(private hhtp: HttpClient) {}
+
+  ngOnInit(): void {
+    this.hhtp.get<Pagination<Product[]>>('https://localhost:5001/api/products').subscribe({
+      next: response => this.products = response.data, // what to do next
+      error: error => console.log(error), //what to do if there is error
+      complete: () => {
+        console.log('request completed');}
+    })
+  }
 }
