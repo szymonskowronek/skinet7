dae01b165dcc2ea83eabad1ef4644b893aa565ed
Szymon Skowronek <szymon.skowronek@globallogic.com>
2024-07-03 14:45:52 +0200

    end of section 10 (routing changes)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,26 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
-import { ShopModule } from './shop/shop.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule
+    HomeModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -31,6 +31,10 @@ export class ShopService {
     });
   }
 
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
+  }
+
   getBrands() {
     return this.http.get<Brand[]>(this.baseUrl + 'products/brands');
   }
