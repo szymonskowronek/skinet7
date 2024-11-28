b99a68a88fb91169166942571588feac537f287e
Szymon Skowronek <szymon.skowronek@globallogic.com>
2024-11-28 13:00:18 +0100

    End of setion 18

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -21,11 +21,16 @@ namespace API.Extensions
             services.AddSingleton<IConnectionMultiplexer>(c => 
             {
                 var options = ConfigurationOptions.Parse(config.GetConnectionString("Redis"));
+                options.AbortOnConnectFail = false;
+                options.ConnectTimeout = 10000;
+                options.SyncTimeout = 10000;
                 return ConnectionMultiplexer.Connect(options);
             });
-            services.AddScoped<IBasketRepository, BasketRepository>();
-            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.Configure<ApiBehaviorOptions>(options =>
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Core.Entities;
 using System.Reflection;
+using Core.Entities.OrderAggregate;
 
 namespace Infrastructure.Data
 {
@@ -15,6 +16,12 @@ namespace Infrastructure.Data
         public DbSet<ProductBrand> ProductBrands { get; set; }
 
         public DbSet<ProductType> ProductTypes { get; set; }    
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        public DbSet<DeliveryMethod> DeliveryMethods { get; set; }     
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
